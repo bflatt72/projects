086f45e201927b0c69f526d1095d481272679e66
bflatt72 <bflatt7t72@gmail.com>
2021-10-16 17:25:24 -0400

    format files

--- projectEuler/problem2.js
+++ projectEuler/problem2.js
@@ -1,17 +1,16 @@
 //Find the sum of even Fibonacci numbers
 
+var x = 0;
+var y = 1;
+var z = 1;
+var sum = 0;
 
-var x=0;
-var y=1;
-var z=1;
-var sum=0;
-
-while (z < 4e6) {
-   if (z%2 == 0) {
-      sum+=z;
-   }
-   x=y;
-   y=z;
-   z = x+y;
+while (z < 100) {
+  if (z % 2 == 0) {
+    sum += z;
+  }
+  x = y;
+  y = z;
+  z = x + y;
 }
 console.log(sum);
--- projectEuler/problem3.js
+++ projectEuler/problem3.js
@@ -1,18 +1,15 @@
 //Largest Prime Factor. Not sure why so many people have tried to make this problem more difficult than it is or tried to do more than the problem asked for but this seems to me to be the simplest solution and you don't even need an extra isPrime function. This solution is very slow for larger numbers though.//
 
-
-
-function largestPrimeFactor(n){
-var i=2;
-while (i <= n) {
+function largestPrimeFactor(n) {
+  var i = 2;
+  while (i <= n) {
     if (n % i === 0) {
-        n/=i;    
+      n /= i;
+    } else {
+      i++;
     }
-    else{
-        i++;
-    }
-}
-console.log(i);
+  }
+  console.log(i);
 }
 
-largestPrimeFactor(600851475143)
+largestPrimeFactor(46568);
