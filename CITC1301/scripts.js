5ec9b24cbfbfee5acf4f54d0e42e751fdf7f7424
bflatt72 <15645106+bflatt72@users.noreply.github.com>
2025-02-26 16:05:36 -0500

    edited blog post card

--- CITC1301/scripts.js
+++ CITC1301/scripts.js
@@ -1,76 +1,50 @@
+function showSocial() {
+  var x = document.getElementById("mySocial");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
 
- 	function showSocial() {
+function showSection() {
+  var x = document.getElementById("resources");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
- 		var x = document.getElementById("mySocial");
- 		if (x.style.display === "flex") {
- 			x.style.display="none";
- 		} else {
- 			x.style.display="flex";
- 		}
- 	}
+function showSection1() {
+  var x = document.getElementById("books");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
+function changePic() {
+  document.getElementById("myPic").src = "me.jpg";
+}
 
+function changePic1() {
+  document.getElementById("myPic").src = "eat.jpg";
+}
 
+function swapBackground() {
+  var a = document.getElementById("body");
+  var x = document.getElementById("pageOne");
+  var y = document.getElementById("pageTwo");
+  var z = document.getElementById("top");
 
-	function showSection() {
-		var x = document.getElementById("resources");
-		if (x.style.display === "block") {
-			x.style.display="none";
-		} else {
-			x.style.display="block";
-		}
-	}
+  a.style.backgroundImage = "url('vintage-concrete.png')";
+  x.style.backgroundImage = "url('topography.png')";
+  y.style.backgroundImage = "url('concrete-texture.png')";
+  z.style.backgroundImage = "url('vintage-concrete.png')";
+}
 
-
-
- 
-
-
-	function showSection1() {
-		var x = document.getElementById("books");
-		if (x.style.display === "block") {
-			x.style.display="none";
-		} else {
-			x.style.display="block";
-		}
-	}
-
-
-
- 
-
-
-
-
-	function changePic() {
-		document.getElementById("myPic").src="me.jpg";
-	}
-
-
-
-	function changePic1() {
-	  document.getElementById("myPic").src="eat.jpg";
-	
-	}
-
-
-
-
-	function swapBackground() {
-		var a = document.getElementById("body");
-		var x = document.getElementById("pageOne");
-		var y = document.getElementById("pageTwo");
-		var z= document.getElementById("top");
-
-		a.style.backgroundImage = "url('vintage-concrete.png')";
-		x.style.backgroundImage = "url('topography.png')";
-		y.style.backgroundImage = "url('concrete-texture.png')";
-	 	z.style.backgroundImage = "url('vintage-concrete.png')";
-	
-	}
-
-
-	function reload() {
-		location.reload();
-	}
-
+function reload() {
+  location.reload();
+}
